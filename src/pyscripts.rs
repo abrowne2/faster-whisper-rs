0afc0fdbc04dafbb3fb8c9088c2f332f97336d27
abrowne2 <adamo.browne@gmail.com>
2025-09-22 12:07:25 -0700

    I[dated

--- src/pyscripts.rs
+++ src/pyscripts.rs
@@ -1,5 +1,4 @@
-
-pub fn get_script() -> String{
+pub fn get_script() -> String {
     r#"
 
 from faster_whisper import WhisperModel
@@ -9,16 +8,16 @@ def new_model(size, device, compute):
     return WhisperModel(size, device=device, compute_type=compute)
 
 def transcribe_audio(
-    model, 
-    path, 
-    prompt, 
-    prefix, 
-    language, 
-    beam_size, 
-    best_of, 
-    patience, 
-    length_penalty, 
-    chunk_length , 
+    model,
+    path,
+    prompt,
+    prefix,
+    language,
+    beam_size,
+    best_of,
+    patience,
+    length_penalty,
+    chunk_length ,
     vad
 ):
     vad_par = dict(
@@ -30,22 +29,23 @@ def transcribe_audio(
     )
 
     segments, _ = model.transcribe(
-        audio=path, 
-        beam_size= beam_size if beam_size > 0 else 5, 
+        audio=path,
+        beam_size= beam_size if beam_size > 0 else 5,
         best_of= best_of if best_of > 0 else 5,
         patience= patience if patience > 0 else 1,
         length_penalty= length_penalty if length_penalty > 0 else 1,
-        language= "en" if language == None else language,
+        language= "en",
         prefix= None if prefix == None else prefix,
         chunk_length= None if chunk_length == "None" else chunk_length,
         initial_prompt= None if prompt == None else prompt,
         vad_filter=vad[0],
         vad_parameters=vad_par,
+        batch_size = 8
     )
 
     segments = list(segments)
     s = []
-    
+
     for segment in segments:
         s.append((
             segment.id,
@@ -58,7 +58,8 @@ def transcribe_audio(
             segment.compression_ratio,
             segment.no_speech_prob,
         ))
-    
+
     return s
-        "#.to_string()
+        "#
+    .to_string()
 }
